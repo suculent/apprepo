8824ab45cee5c5bda762e82e9d7fbb83db160497
Matěj Sychra <igraczech@igraczech.com>
2016-05-20 10:04:06 +0200

    customizing circle

--- spec/uploader_spec.rb
+++ spec/uploader_spec.rb
@@ -24,14 +24,3 @@ describe AppRepo do
     end
   end
 end
-
-# SAMPLE ONLY
-#require "./greeter.rb"
-
-#describe "Greeter" do
-#  it "should say 'Hello World!' when it receives the greet() message" do
-#    greeter = Greeter.new
-#    greeting = greeter.greet
-#    expect(greeting).to eq "Hello World!"
-#  end
-#end
