563e15567640afca3befd2af484a45c84aa159f8
Matěj Sychra <igraczech@igraczech.com>
2016-05-22 23:10:11 +0200

    upload descriptor should have all required data now

--- lib/apprepo/upload_descriptor.rb
+++ lib/apprepo/upload_descriptor.rb
@@ -1,10 +1,13 @@
 module AppRepo
   class UploadDescriptor
-    attr_accessor :appcode
+
+    attr_accessor :appcode # required
+    attr_accessor :ipa # can be inferred anyway (glob or metadata)
+    attr_accessor :metadata # optional, allows re-uploading same binary without metadata change
 
     def initialize(appcode)
       self.appcode = appcode
-      puts 'Initializing "AppRepo:UploadDescriptor with appcode "' + self.appcode + '"'
+      UI.message('Initializing "AppRepo:UploadDescriptor with appcode "' + self.appcode + '"')
     end
   end
 end
