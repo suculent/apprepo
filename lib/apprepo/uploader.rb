abfe48b3310198aa224be6b607841931e060475d
Matěj Sychra <igraczech@igraczech.com>
2016-05-22 23:09:52 +0200

    added upload_descriptor attribute to uploader

--- lib/apprepo/uploader.rb
+++ lib/apprepo/uploader.rb
@@ -6,46 +6,76 @@ require_relative 'upload_descriptor'
 
 module AppRepo
   class Uploader
+
     attr_accessor :host
-
     attr_accessor :login
-
     attr_accessor :keypath
+    attr_accessor :appcode
+    attr_accessor :upload_descriptor
 
-    def initialize(host, login, keypath)
+    attr_accessor :ipa
+  
+
+    def initialize(host, login, keypath, appcode)
       self.host = host
       self.login = login
       self.keypath = keypath
-      puts 'Initializing "AppRepo:Uploader"'
+      self.appcode = appcode
+
+      UI.message('[AppRepo:Uploader] Initializing...')
+
+
+      path = File.dirname(__FILE__)+'/../../*.ipa'
+      puts path
+
+      self.ipa = Dir.glob(path).last
+      puts ipa
+
+      
+      UI.message('[DEBUG]:' + self.host + ' | ' + self.login + ' | ' + self.keypath + ' | ' + self.appcode )
     end
 
     def run
+
+      if !appcode 
+        UI.user_error('APPCODE value missing.')
+        exit 0
+      end
+
+      if !ipa
+        UI.user_error('IPA value missing.')
+        exit 0
+      end
+
       File.open(keypath, 'r') do |file|
-        puts '[AppRepo:Uploader] reading private key...'
+        UI.message('[AppRepo:Uploader] reading private key...')
 
         rsa_key = [file.read]
 
-        puts '[AppRepo:Uploader] starting SSH connection...'
+        UI.message('[AppRepo:Uploader] starting SSH connection...')
 
         Net::SSH.start(host, login, password: 'circle') do |ssh|
           # Net::SSH.start( self.host, self.login, :key_data => rsa_key, :keys_only => true) do |ssh|
 
-          puts '[AppRepo:Uploader] logging to AppRepo...'
+          UI.message('[AppRepo:Uploader] logging to AppRepo...')
 
           ssh.sftp.connect do |sftp|
-            puts '[AppRepo:Uploader] AppRepo successfully connected...'
 
-            puts '[AppRepo:Uploader] TODO: Traverse to correct "APPCODE" folder...'
+            UI.message('[AppRepo:Uploader] AppRepo successfully connected...')
 
-            result = ssh.exec!('cd repo/apps; ls')
-            puts result
+            UI.message('[AppRepo:Uploader] TODO: Traverse to correct "APPCODE" folder...')
+
+            result = ssh.exec!('cd repo/apps/' + self.appcode + '; ls')
+            UI.message(result)
+
+            UI.message('Will try to upload ' + self.ipa )
 
             # upload a file or directory to the remote host
-            sftp.upload!('/Users/sychram/test.data', '/home/circle/repo/test.data')
+            sftp.upload!( self.ipa , '/home/circle/repo/test.data')
 
             result = ssh.exec!('ls')
 
-            puts result
+            UI.message(result)
 
             remote = '/home/circle/repo/test.data'
             local = '/Users/sychram/test.data.from-remote'
@@ -75,7 +105,8 @@ module AppRepo
             # verify if this returns 11. Your server may return
             # something different like 4.
             if e.code == 11
-              puts 'directory already exists. Carry on...'
+              # warning?
+              UI.user_error('directory already exists. Carry on...')
               sftp.rmdir!('/home/circle/ruby-test')
             else
               raise
