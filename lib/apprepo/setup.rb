428af8bff1815c7b069fff574f41f862842b7268
Matěj Sychra <igraczech@igraczech.com>
2016-05-23 18:30:29 +0200

    adjusting setup based on Repofile

--- lib/apprepo/setup.rb
+++ lib/apprepo/setup.rb
@@ -1,20 +1,14 @@
 module AppRepo
   class Setup
-    def run(options)
-      UI.message('[AppRepo:Setup] Running...')
-      containing = (File.directory?('fastlane') ? 'fastlane' : '.')
-      file_path = File.join(containing, 'Repofile')
-      data = generate_apprepo_file(containing, options)
-      setup_apprepo(file_path, data, containing, options)
-    end
 
     def setup_apprepo(file_path, data, _apprepo_path, _options)
+      UI.message('[AppRepo:Setup] Setting up...')
       File.write(file_path, data)
 
       # TODO: implement later
       # download_metadata(apprepo_path, options)
 
-      UI.success("Successfully created new Repofile at path '#{file_path}'")
+      UI.success("NOT! created new Repofile at path '#{file_path}'")
     end
 
     # This method takes care of creating a new 'apprepo' folder, containg the app metadata
@@ -32,7 +26,7 @@ module AppRepo
       apprepo.gsub!("[[APP_VERSION]]", options[:app].version)
       apprepo.gsub!("[[APP_NAME]]", options[:app].name)
       UI.user_error("TODO: ADJUST Repofile'")
-      apprepo
+      # deliver => apprepo??
     end
 
     def download_metadata(apprepo_path, _options)
@@ -40,6 +34,14 @@ module AppRepo
       FileUtils.mkdir_p(path)
       UI.success("TODO: DOWNLOAD METADATA'")
       # AppRepo::DownloadManifest.run(options, path)
+    end
+
+    def run(options)
+      UI.message('[AppRepo:Setup] Running...')
+      containing = (File.directory?('fastlane') ? 'fastlane' : '.')
+      file_path = File.join(containing, 'Repofile')
+      data = generate_apprepo_file(containing, options)
+      setup_apprepo(file_path, data, containing, options)
     end
   end
 end
