1b50760e0e13f353c250f127631d482666231977
Matěj Sychra <igraczech@igraczech.com>
2016-05-24 23:11:48 +0200

    version bump, disabled rspec until resolved (works locally)

--- lib/apprepo/version.rb
+++ lib/apprepo/version.rb
@@ -1,4 +1,4 @@
 module AppRepo
-  VERSION = '0.0.4'.freeze
+  VERSION = '0.0.5'.freeze
   DESCRIPTION = 'Upload icon, metadata and your app to the T-Mobile Enterprise AppRepo and notify users on updaet using a single command'.freeze
 end
