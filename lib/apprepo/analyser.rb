a19db55a3caafbd1d60cb8a45477713caa95594c
Matěj Sychra <igraczech@igraczech.com>
2016-05-25 17:28:59 +0200

    added missing methods to Analyser

--- lib/apprepo/analyser.rb
+++ lib/apprepo/analyser.rb
@@ -2,7 +2,13 @@ module AppRepo
     
   # Should provide metadata from current context
   class Analyser
+    
+    def fetch_app_version
+        return "0.0"
+    end
+    
     def run
-    end  
+    end
+    
   end
 end
