9fde064b3a9cab1ba804dc1262285c430d9f4432
Matěj Sychra <igraczech@igraczech.com>
2016-05-26 15:30:34 +0200

    version bump

--- lib/apprepo/version.rb
+++ lib/apprepo/version.rb
@@ -1,5 +1,5 @@
 module AppRepo
-  VERSION = "0.0.9".freeze
+  VERSION = "0.1.0".freeze
   DESCRIPTION = "Upload icon, manifest.json and your app to SFTP server
    and notify users on updaet using a single command".freeze
   SUMMARY = "Upload icon, manifest and your app to SFTP".freeze
